cc4ced42813a35c7b53c1b079d01d5ba5fed92bc
MoeFingers <mbzuiter@gmail.com>
2024-01-25 21:11:17 -0700

    loaded and tested fake data

--- public/index.js
+++ public/index.js
@@ -4,6 +4,15 @@ async function main() {
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
 
+
+    // get data
+    let data = mockData
+
+    const { GME, MSFT, DIS, BNTX } = data;
+
+    const stocks = [GME, MSFT, DIS, BNTX]
+
+    console.log(stocks)
 }
 
 main()
